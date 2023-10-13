a7920f872f56d8ece02c257f8696fac9660213e4
beerbumm <chelseaw.gfx@gmail.com>
2023-10-13 11:46:32 -0700

    updated html. Added selection timezones in javascript

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ function updateTime() {
   // Copenhagen
   let copenhagenElement = document.querySelector("#copenhagen");
   if (copenhagenElement) {
-    let copenghagenDateElement = copenhagenElement.querySelector(".date");
+    let copenhagenDateElement = copenhagenElement.querySelector(".date");
     let copenhagenTimeElement = copenhagenElement.querySelector(".time");
 
-    let copenhagaenTime = moment().tz("Europe/Copenhagen");
+    let copenhagenTime = moment().tz("Europe/Copenhagen");
 
-    copenghagenDateElement.innerHTML = copenhagaenTime.format("MMMM Do, YYYY");
-    copenhagenTimeElement.innerHTML = copenhagaenTime.format(
+    copenhagenDateElement.innerHTML = copenhagenTime.format("MMMM Do, YYYY");
+    copenhagenTimeElement.innerHTML = copenhagenTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
@@ -42,5 +42,26 @@ function updateTime() {
   }
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
