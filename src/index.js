0c36d5347912a0ec64d6180032a54cebdf817cb6
beerbumm <chelseaw.gfx@gmail.com>
2023-10-13 10:46:06 -0700

    cleaned up html and js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,45 @@
 function updateTime() {
   // Copenhagen
   let copenhagenElement = document.querySelector("#copenhagen");
-  let copenghagenDateElement = copenhagenElement.querySelector(".date");
-  let copenhagenTimeElement = copenhagenElement.querySelector(".time");
+  if (copenhagenElement) {
+    let copenghagenDateElement = copenhagenElement.querySelector(".date");
+    let copenhagenTimeElement = copenhagenElement.querySelector(".time");
 
-  let copenhagaenTime = moment().tz("Europe/Copenhagen");
+    let copenhagaenTime = moment().tz("Europe/Copenhagen");
 
-  copenghagenDateElement.innerHTML = copenhagaenTime.format("MMMM Do, YYYY");
-  copenhagenTimeElement.innerHTML = copenhagaenTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    copenghagenDateElement.innerHTML = copenhagaenTime.format("MMMM Do, YYYY");
+    copenhagenTimeElement.innerHTML = copenhagaenTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Seattle
   let seattleElement = document.querySelector("#seattle");
-  let seattleDateElement = seattleElement.querySelector(".date");
-  let seattleTimeElement = seattleElement.querySelector(".time");
+  if (seattleElement) {
+    let seattleDateElement = seattleElement.querySelector(".date");
+    let seattleTimeElement = seattleElement.querySelector(".time");
 
-  let seattleTime = moment().tz("America/Los_Angeles");
+    let seattleTime = moment().tz("America/Los_Angeles");
 
-  seattleDateElement.innerHTML = seattleTime.format("MMMM Do, YYYY");
-  seattleTimeElement.innerHTML = seattleTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    seattleDateElement.innerHTML = seattleTime.format("MMMM Do, YYYY");
+    seattleTimeElement.innerHTML = seattleTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Melbourne
   let melbourneElement = document.querySelector("#melbourne");
-  let melbourneDateElement = melbourneElement.querySelector(".date");
-  let melbourneTimeElement = melbourneElement.querySelector(".time");
+  if (melbourneElement) {
+    let melbourneDateElement = melbourneElement.querySelector(".date");
+    let melbourneTimeElement = melbourneElement.querySelector(".time");
 
-  let melbourneTime = moment().tz("Australia/Melbourne");
+    let melbourneTime = moment().tz("Australia/Melbourne");
 
-  melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do, YYYY");
-  melbourneTimeElement.innerHTML = melbourneTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do, YYYY");
+    melbourneTimeElement.innerHTML = melbourneTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
